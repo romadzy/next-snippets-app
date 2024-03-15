d0e1f7fb239d312c4af35e29041cdf099aaf9789
Roman Dziuba <dzyuba.roman09@gmail.com>
2024-03-15 18:34:35 +0200

    added validation and error handling for the create snippet page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -25,8 +25,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
       <h1 className="text-xl font-bold">{snippet.title}</h1>
       <div className="flex gap-4">
         <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-        <form action={deleteSnippetAction}></form>
-        <button type="submit" className="p-2 border rounded">Delete</button>
+        <form action={deleteSnippetAction}>
+          <button type="submit" className="p-2 border rounded">Delete</button>
+        </form>
       </div>
     </div>
     <pre className="p-3 border rounded bg-gray-200 border-gray-200">
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,26 +1,12 @@
-import { redirect } from "next/navigation";
-import { db } from "@/db"
+'use client'
+
+import { useFormState } from "react-dom"
+import * as actions from '@/actions'
 
 export default function SnippetCreatePage() {
-  const createSnippet = async (formData: FormData) => {
-    // This needs to be a server action
-    'use server';
+  const [formState, action] = useFormState(actions.createSnippet, {message: ''});
 
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code
-      }
-    });
-    console.log(snippet);
-
-    redirect('/');
-  }
-
-  return <form action={createSnippet}>
+  return <form action={action}>
     <h3 className="font-bold m-3">Create a Snippet</h3>
     <div className="flex flex-col gap-4">
       <div className="flex gap-4">
@@ -44,6 +30,10 @@ export default function SnippetCreatePage() {
           id="code"
         />
       </div>
+
+      {
+        formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null
+      }
 
       <button type="submit" className="rounded p-2 bg-blue-200">
         Create
