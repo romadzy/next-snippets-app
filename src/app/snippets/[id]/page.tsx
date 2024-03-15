ab697d4292d61a833fc686d1e0c0b3fbd4ca10ea
Roman Dziuba <dzyuba.roman09@gmail.com>
2024-03-15 15:57:06 +0200

    added snippet editing/deleting functionality

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "@/db";
+import * as actions from '@/actions';
 
 interface SnippetShowPageProps {
   params: {
@@ -16,5 +18,19 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
-  return <div>{snippet.title}</div>
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
+  return <div>
+    <div className="flex m-4 justify-between items-center">
+      <h1 className="text-xl font-bold">{snippet.title}</h1>
+      <div className="flex gap-4">
+        <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+        <form action={deleteSnippetAction}></form>
+        <button type="submit" className="p-2 border rounded">Delete</button>
+      </div>
+    </div>
+    <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+      <code>{snippet.code}</code>
+    </pre>
+  </div>
 }
